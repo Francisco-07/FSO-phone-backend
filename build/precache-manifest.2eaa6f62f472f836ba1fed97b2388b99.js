08676626c7033f369c88c99a71d40a818e77a484
Francisco Garciarena <francisco.garciarena94@gmail.com>
2023-01-16 19:24:55 -0300

    added db and other functions

---
+++ build/precache-manifest.2eaa6f62f472f836ba1fed97b2388b99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6de8616ffa793dfac9ce64f64e0182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d28718c1692bad60166",
+    "url": "/static/js/2.a07936eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.a07936eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf2d34374cf28bf03b3b",
+    "url": "/static/js/main.ece09b84.chunk.js"
+  },
+  {
+    "revision": "d7a834a4681d594935b1",
+    "url": "/static/js/runtime-main.714592f8.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const Person = require('./models/person')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -6,99 +8,95 @@ app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
-
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0
-  return maxId + 1
-}
-
-app.get('/', (request, response) => {
-  response.send('<h1>Phonebook backend</h1>')
-})
-
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({})
+    .then((persons) => {
+      response.json(persons)
+    })
+    .catch((error) => next(error))
 })
 
-app.get('/info', (request, response) => {
-  const currentDate = new Date().toString()
-
-  response.send(
-    `<div>
-        <h2>Phonebook has info for ${persons.length} people</h2>
-    </div> 
-    <div>
-        <p>${currentDate}</p>
-    </div>`
-  )
+app.get('/info', (request, response, next) => {
+  const currentDate = new Date().toLocaleString()
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+  Person.find({})
+    .then((persons) => {
+      response.send(
+        `
+            <h2>
+                <p>Phonebook has info for ${persons.length} people</p>
+            </h2>
+            <p>
+                <p>${currentDate} (${timeZone})</p>
+            </p>`
+      )
+    })
+    .catch((error) => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = persons.find((note) => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      response.json(person)
+    })
+    .catch((error) => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((note) => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  const nameExist = persons.find((person) => person.name === body.name)
-
-  if (nameExist) {
-    return response.status(400).json({
-      error: 'name must be unique',
-    })
+  if (body.name === '') {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'content missing',
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson)
     })
-  }
+    .catch((error) => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
   const person = {
-    id: generateId(),
     name: body.name,
     number: body.number,
   }
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson.toJSON())
+    })
+    .catch((error) => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
